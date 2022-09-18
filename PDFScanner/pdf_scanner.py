296ed7f462c75c269b2fe237efe30d4a6d4e0803
Jon <jonhops2@gmail.com>
2022-09-18 14:59:40 -0400

    Changed to while loop; perfect top half scanned

--- PDFScanner/pdf_scanner.py
+++ PDFScanner/pdf_scanner.py
@@ -1,7 +1,7 @@
 from PyPDF2 import PdfReader
 
 top_keywords_allowance_claiming = ( #Keywords for allowance claiming races
-    ("track", "", "-"),
+    ("track", "", "-", 1),
     ("date" ,  "-", "-"),
     ("race_num" ,  "-", "ALLOWANCE"),
     ("criteria" ,  "FOR", ".", 1),
@@ -30,7 +30,9 @@ text = page.extract_text()
 tokenized_text = text.split()
 
 keyword_count = 0
-for i in range(0, len(tokenized_text)):
+i = -1
+while i < len(tokenized_text) - 1:
+    i+=1 #Increment at start of loop
     keyword_tuple = top_keywords_allowance_claiming[keyword_count]
     if tokenized_text[i].__contains__(keyword_tuple[1]): #If startword is found
         value_string = "" #Start of string
@@ -46,11 +48,13 @@ for i in range(0, len(tokenized_text)):
             next_word = tokenized_text[i]
         if len(keyword_tuple) > 3 and keyword_tuple[3] != 1: #Checking if end is inclusive
             value_string = value_string + " " + next_word
+        else: #End is exclusive, decrement by 1 to start at this word next loop
+            i -= 1
         top_fields.update({keyword: value_string}) #Add to dictionary
         keyword_count+=1 #Increment to next keyword
-    if keyword_count >= 10: #Check if keywords are filled
+    if keyword_count >= len(top_keywords_allowance_claiming): #Check if keywords are filled
         break
 
 
-print(tokenized_text)
-print(top_fields)
+#print(tokenized_text)
+#print(top_fields)
